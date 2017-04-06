b4d39391820e9f074ecdc4590716e989ac3413d3
Alan Nguyen <nguyen.alan@me.com>
2017-04-06 08:47:01 -0400

    basic functionality stubs for requesting service and finding an elevator

--- elevator.rb
+++ elevator.rb
@@ -1,9 +1,35 @@
  class Elevator
-  def initialize
-    @id = 1
+  attr_reader :current_floor, :destinations, :maintenance_mode
+
+  def initialize(options)
+    @id = options[:id]
     @current_floor = 1
     @destination = nil
     @trips = 0
     @maintenance_mode = false
+  end
+
+  def add_destination(destination)
+    @destination = destination
+  end
+
+  def move_up
+    @current_floor += 1
+    notify_floor_change
+  end
+
+  def move_down
+    @current_floor -= 1
+    notify_floor_change
+  end
+
+  def notify_floor_change
+    # Notifies listeners that the elevator has reached a new floor
+    # Uses @id and @current_floor to provide enough context for listeners
+  end
+
+  def notify_door_status
+    # Notifies listeners that the elevator's doors are opening/closing
+    # Uses @id and __status__ to provide enough context for listeners
   end
  end
--- elevator_controller.rb
+++ elevator_controller.rb
@@ -4,4 +4,39 @@ class ElevatorController
     @bottom_floor = options[:bottom_floor] || 1
     @top_floor = options[:top_floor]
   end
+
+  def install_elevator
+    @elevators.push(Elevator.new({id: @elevators.count}))
+  end
+
+  def request_service(source, destination)
+    # elevators can't go beyond top floor, below bottom floor, and no action for same floor
+    return if destination > @top_floor || destination < @bottom_floor || source == destination
+    # Finds elevator, requests
+    elevator = find_some_elevator(source, destination)
+    elevator.add_destination(destination)
+  end
+
+  private
+
+  def active_elevators
+    # returns list of elevators not in maintenance mode
+    @elevators.select{|elevator| !elevator.maintenance_mode}
+  end
+
+  def find_some_elevator(source, destination)
+
+  end
+
+  def find_on_floor(floor)
+    # Finds an elevator on the same floor
+  end
+
+  def find_in_flight(source, destination)
+    # Finds an elevator that will be passing source
+  end
+
+  def find_nearest(source)
+    # Finds nearest elevator, not in flight
+  end
 end
